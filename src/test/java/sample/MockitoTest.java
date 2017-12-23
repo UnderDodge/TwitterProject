691fa169f50cd80b6fce6118853b665a11ac20d5
UnderDodge <assassinthug009@gmail.com>
2017-12-23 20:40:02 +0200

    first mock tests

---
+++ src/test/java/sample/MockitoTest.java
@@ -0,0 +1,32 @@
+package sample;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.instanceOf;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+/**
+ * Created by Администратор on 23.12.2017.
+ */
+public class MockitoTest {
+    TwitterManager tm;
+
+    @Before
+    public void create(){
+        tm = mock(TwitterManager.class);
+        when(tm.pow(2,5)).thenReturn(25.0);
+        when(tm.getTimeline()).thenReturn(new StringBuffer("not connected to Twitter API, this is just a mock test"));
+    }
+
+    @Test
+    public void powTest(){
+        assertTrue(25.0 == tm.pow(2, 5));
+    }
+
+    @Test
+    public void timeLineTestBeforeConnection(){
+        assertThat(tm.getTimeline(),instanceOf(StringBuffer.class));
+    }
+}
